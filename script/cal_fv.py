de260585aceeba61c192e837665d43f61c7d3776
Zifeng Yu <alan@yanhuangdata.com>
2021-08-09 10:49:33 +0800

    backup

--- script/cal_fv.py
+++ script/cal_fv.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     
     a = (1 + float(g)/ 100) / (1 + float(r)/ 100)
     
-    for n in [5, 10]:
+    for n in [5, 10, 20, 30]:
         total = 0
         for i in range(1, n+1):
             total += a ** i
